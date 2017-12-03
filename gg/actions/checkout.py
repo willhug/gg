dcc1c48241b6dd0335a417c5a207d4d377231c28
Will Hughes <willhughes01@gmail.com>
2017-12-02 17:36:07 -0800

    Move checkout code to actions folder

---
+++ gg/actions/checkout.py
@@ -0,0 +1,51 @@
+from typing import Optional
+
+from gg.gateways.git.branch_checkout import git_checkout
+from gg.gateways.git.branch_info import get_current_branch
+from gg.lib.branch_name import (
+    get_branch_for_feature_part,
+    get_first_branch_for_feature,
+    get_next_branch,
+    get_previous_branch,
+    parse_branch_name,
+)
+from gg.lib.log import logger
+
+def checkout(
+    direction: Optional[bool] = None, # True is forward, False is backwards
+    part: Optional[float] = None,
+    feature: Optional[str] = None,
+) -> int:
+    if direction == True:
+        current_branch = get_current_branch()
+        if not current_branch:
+            logger.error('no branch currently checked out, cannot checkout relative commit')
+            return 1
+        branch_to_checkout = get_next_branch(current_branch)
+    elif direction == False:
+        current_branch = get_current_branch()
+        if not current_branch:
+            logger.error('no branch currently checked out, cannot checkout relative commit')
+            return 1
+        branch_to_checkout = get_previous_branch(current_branch)
+    elif part is not None and feature is not None:
+        branch_to_checkout = get_branch_for_feature_part(feature, part)
+    elif part is not None:
+        current_branch = get_current_branch()
+        if not current_branch:
+            logger.error('no branch currently checked out, cannot checkout relative commit')
+            return 1
+        branch = parse_branch_name(current_branch)
+        branch_to_checkout = get_branch_for_feature_part(branch.feature, part)
+    elif feature is not None:
+        branch_to_checkout = get_first_branch_for_feature(feature)
+    else:
+        logger.error('cannot determine branch to checkout')
+        return 1
+
+    if branch_to_checkout is None:
+        logger.error('cannot determine branch to checkout')
+        return 1
+
+    git_checkout(branch_to_checkout)
+    return 0
--- gg/cli/subcommands/checkout.py
+++ gg/cli/subcommands/checkout.py
@@ -1,16 +1,7 @@
 from plumbum import cli
 
+from gg.actions.checkout import checkout
 from gg.cli.gg import GG
-from gg.gateways.git.branch_checkout import git_checkout
-from gg.gateways.git.branch_info import get_current_branch
-from gg.lib.branch_name import (
-    get_branch_for_feature_part,
-    get_first_branch_for_feature,
-    get_next_branch,
-    get_previous_branch,
-    parse_branch_name,
-)
-from gg.lib.log import logger
 
 @GG.subcommand("co")
 class GGCheckout(cli.Application):
@@ -22,41 +13,15 @@ class GGCheckout(cli.Application):
     feature_name = cli.SwitchAttr(['-f', '--feature'], str, help="base branch name to check out", default=None)
 
     def main(self, *args):
+        direction = None
         if self.getNext:
-            current_branch = get_current_branch()
-            if not current_branch:
-                logger.error('no branch currently checked out, cannot checkout relative commit')
-                return 1
-            branch_to_checkout = get_next_branch(current_branch)
+            direction = True
         elif self.getPrev:
-            current_branch = get_current_branch()
-            if not current_branch:
-                logger.error('no branch currently checked out, cannot checkout relative commit')
-                return 1
-            branch_to_checkout = get_previous_branch(current_branch)
-        elif self.part is not None and self.feature_name is not None:
-            branch_to_checkout = get_branch_for_feature_part(self.feature_name, self.part)
-        elif self.part is not None:
-            current_branch = get_current_branch()
-            if not current_branch:
-                logger.error('no branch currently checked out, cannot checkout relative commit')
-                return 1
-            branch = parse_branch_name(current_branch)
-            branch_to_checkout = get_branch_for_feature_part(branch.feature, self.part)
-        elif self.feature_name is not None:
-            branch_to_checkout = get_first_branch_for_feature(self.feature_name)
-        else:
-            logger.error('cannot determine branch to checkout')
-            return 1
+            direction = False
 
-        return self.checkout_branch(branch_to_checkout)
-
-    def checkout_branch(self, branch_name: str) -> int:
-        if branch_name is None:
-            logger.error('cannot determine branch to checkout')
-            return 1
-
-        git_checkout(branch_name)
-
-        return 0
+        return checkout(
+            direction=direction,
+            part=self.part,
+            feature=self.feature_name,
+        )
 
