bd704e72a00971b9959b3412877a55ba1e3b063e
Will Hughes <william.hughes@samsara.com>
2019-06-19 11:48:56 -0700

    Add origin/master for fetching, remove interactive options that don't work

--- gg/gateways/git/fetch.py
+++ gg/gateways/git/fetch.py
@@ -4,5 +4,5 @@ from gg.gateways.git import git_cmd
 
 def git_fetch() -> int:
     """Delete a git branch"""
-    command = git_cmd['fetch', '-p']
+    command = git_cmd['fetch', '-p', 'origin', 'master']
     return (command) & FG
--- gg/gateways/git/rebase.py
+++ gg/gateways/git/rebase.py
@@ -4,5 +4,5 @@ from gg.gateways.git import git_cmd
 
 def git_rebase_interactive(ref: str) -> int:
     """perform an interactive rebase onto the ref"""
-    command = git_cmd['rebase', '--committer-date-is-author-date', '--ignore-date', '-i', ref]
+    command = git_cmd['rebase', '-i', ref]
     return (command) & FG
