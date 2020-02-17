9446cd6171554cb8726a4012156924520b9b8089
Will Hughes <william.hughes@samsara.com>
2020-02-17 13:19:29 -0800

    Add fixup for fixing commit dates.

--- gg/cli/subcommands/fixup.py
+++ gg/cli/subcommands/fixup.py
@@ -2,7 +2,7 @@ from plumbum import cli, colors
 
 from gg.cli.gg import GG
 from gg.gateways.git.branch_info import get_current_branch
-from gg.gateways.git.rebase import git_rebase_interactive
+from gg.gateways.git.rebase import git_rebase_interactive, git_rebase_commit_order
 from gg.lib.branch_name import get_prefix_branch_name
 
 
@@ -11,9 +11,13 @@ class GGFixup(cli.Application):
     DESCRIPTION = """
 Fixup commits on the stack.
 """
+    time = cli.Flag(['-t', '--timefix'], help="fix the timings on the branch")
 
     def main(self, *args):
+
         current_branch = get_current_branch()
         prefix_branch = get_prefix_branch_name(current_branch)
+        if self.time:
+            return git_rebase_commit_order(prefix_branch)
 
         return git_rebase_interactive(prefix_branch)
--- gg/gateways/git/rebase.py
+++ gg/gateways/git/rebase.py
@@ -6,3 +6,8 @@ def git_rebase_interactive(ref: str) -> int:
     """perform an interactive rebase onto the ref"""
     command = git_cmd['rebase', '-i', ref]
     return (command) & FG
+
+def git_rebase_commit_order(ref: str) -> int:
+    """perform an interactive rebase onto the ref"""
+    command = git_cmd['rebase', ref, '--exec', 'git commit --amend --reset-author --no-edit']
+    return (command) & FG
