fa22dc20e8f6d287c75546cf3adc697603ae21fa
Will Hughes <william.hughes@samsara.com>
2020-10-11 16:05:38 -0700

    go config: Add ability to provide a config file

--- go/src/github.com/willhug/gg/config/config.go
+++ go/src/github.com/willhug/gg/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"github.com/samsarahq/go/oops"
+)
+
+const GGConfigFile = "~/.ggconfig"
+
+type Config struct {
+	GithubAuthToken string `json:"github_auth_token"`
+}
+
+func ReadConfig() (*Config, error) {
+	file, err := os.Open(GGConfigFile)
+	if err != nil {
+		return nil, oops.Wrapf(err, "could not open config")
+	}
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, oops.Wrapf(err, "could not read file")
+	}
+	var c Config
+	if err = json.Unmarshal(b, &c); err != nil {
+		return nil, oops.Wrapf(err, "could not unmarshal")
+	}
+	return &c, nil
+}
