5da9c2327e796a7c8d6b6082a8960c5d4c0207b9
Will Hughes <willhughes01@gmail.com>
2021-09-30 20:33:42 -0700

    [rust] Create config file when none exists

--- rust/src/config.rs
+++ rust/src/config.rs
@@ -1,10 +1,10 @@
 
 use serde::{Deserialize, Serialize};
-use std::io::Read;
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::process::Command;
 use std::str::from_utf8;
-use std::fs::File;
-use std::path::{Path};
+use std::fs::{File, OpenOptions};
+use std::path::{Path, PathBuf};
 
 
 #[derive(Serialize, Deserialize)]
@@ -14,7 +14,13 @@ pub struct Config {
 
 pub fn get_config() -> Config {
     let path = Path::new(get_repo_root_path().as_str()).join(".git").join("GG_CONFIG");
-    let mut file = File::open(path).expect("WANT IT!!!");
+    let file = File::open(path.clone());
+
+    let mut file = match file {
+        Ok(file) => file,
+        Err(_error) => create_config(path),
+    };
+
     let mut buf = String::new();
     file.read_to_string(&mut buf).expect("no error reading!");
     let c: Config = serde_json::from_str(buf.as_str()).expect("res!");
@@ -34,4 +40,36 @@ fn get_repo_root_path() -> String {
         .expect("msg")
         .trim_end_matches(x);
     return result.to_string()
+}
+
+fn create_config(path: PathBuf) -> File {
+    // Query for main/master info
+    println!("Config file not found, creating one at {}", path.to_str().expect("should have been str"));
+    println!("Please input what the main branch is [main|master]: ");
+
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+
+    let x: &[_] = &[' ', '\t', '\n', '\r'];
+    let line = line.trim_end_matches(x);
+
+    let config = Config {
+        repo_main_branch: line.to_string(),
+    };
+
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path)
+        .unwrap();
+
+    let cfg_json = serde_json::to_string(&config).expect("error serializing");
+
+    file.write_all(cfg_json.as_bytes()).expect("could not write to cfg file");
+    file.sync_all().expect("should be able to write to disk");
+    file.seek(SeekFrom::Start(0)).unwrap();
+
+    return file;
 }
