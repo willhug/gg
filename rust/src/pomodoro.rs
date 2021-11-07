786565f5b6f9e593b0ddb007cb89dee70ddd9bcb
Will Hughes <willhughes01@gmail.com>
2021-11-06 20:25:27 -0700

    [rust] Add pomodoro command

---
+++ rust/src/pomodoro.rs
@@ -0,0 +1,24 @@
+use ansi_escapes::Beep;
+
+use std::{io::{Write, stdout}, thread, time};
+
+use crossterm::{QueueableCommand, cursor};
+
+
+pub fn run_pomodoro(duration_min: u32) {
+    println!("Running Pomodoro for {} mins", duration_min);
+
+    let mut stdout = stdout();
+    for i in 0..duration_min {
+        stdout.queue(cursor::SavePosition).unwrap();
+        stdout.write_all(format!("{} minutes left", duration_min - i).as_bytes()).unwrap();
+        stdout.flush().unwrap();
+        thread::sleep(time::Duration::from_secs(60));
+        stdout.queue(cursor::RestorePosition).unwrap();
+        stdout.flush().unwrap();
+    }
+    for _ in 1..10 {
+        print!("{}", Beep);
+        thread::sleep(time::Duration::from_secs(1));
+    }
+}
