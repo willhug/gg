55592d2baf775eff044de8bd946c9cdecd82fab1
Will Hughes <willhughes01@gmail.com>
2022-03-27 19:17:59 -0700

    [status] Don't hardcode status table location

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,11 +1,9 @@
-
 use serde::{Deserialize, Serialize};
+use std::fs::{File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom, Write};
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::str::from_utf8;
-use std::fs::{File, OpenOptions};
-use std::path::{Path, PathBuf};
-
 
 #[derive(Debug)]
 pub struct FullConfig {
@@ -13,8 +11,8 @@ pub struct FullConfig {
     pub repo_name: String,
     pub current_github_user: String,
     pub github_token: String,
-    pub status_file: String,
-    pub status_file_backup_dir: String,
+    pub status_file: PathBuf,
+    pub status_file_backup_dir: PathBuf,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -24,7 +22,7 @@ pub struct SavedConfig {
     pub branch_prefix: String,
     pub repo_org: String,
 
-    #[serde(default="default_split")]
+    #[serde(default = "default_split")]
     pub branch_split: String,
 }
 
@@ -33,28 +31,28 @@ fn default_split() -> String {
 }
 
 pub fn get_full_config() -> FullConfig {
+    let homedir = dirs::home_dir().unwrap();
     FullConfig {
         saved: get_saved_config(),
         repo_name: get_repo_name(),
         current_github_user: "willhug".to_string(),
         github_token: std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env var is required"),
-        status_file: "/home/will/status.txt".to_string(),
-        status_file_backup_dir: "/home/will/status_bu".to_string(),
+        status_file: homedir.join("status.txt"),
+        status_file_backup_dir: homedir.join("status_bu"),
     }
 }
 
 fn get_repo_name() -> String {
     let out = match Command::new("git")
-            .arg("rev-parse")
-            .arg("--show-toplevel")
-            .output() {
-                Ok(output) => output,
-                Err(_e) => panic!("error!")
+        .arg("rev-parse")
+        .arg("--show-toplevel")
+        .output()
+    {
+        Ok(output) => output,
+        Err(_e) => panic!("error!"),
     };
     let x: &[_] = &[' ', '\t', '\n', '\r'];
-    let result = from_utf8(&out.stdout)
-        .expect("msg")
-        .trim_end_matches(x);
+    let result = from_utf8(&out.stdout).expect("msg").trim_end_matches(x);
     result.split('/').last().unwrap().to_string()
 }
 
@@ -74,21 +72,22 @@ pub fn get_saved_config() -> SavedConfig {
 }
 
 fn get_saved_config_file_path() -> PathBuf {
-    Path::new(get_repo_root_path().as_str()).join(".git").join("GG_CONFIG")
+    Path::new(get_repo_root_path().as_str())
+        .join(".git")
+        .join("GG_CONFIG")
 }
 
 fn get_repo_root_path() -> String {
     let out = match Command::new("git")
-            .arg("rev-parse")
-            .arg("--show-toplevel")
-            .output() {
-                Ok(output) => output,
-                Err(_e) => panic!("error!")
+        .arg("rev-parse")
+        .arg("--show-toplevel")
+        .output()
+    {
+        Ok(output) => output,
+        Err(_e) => panic!("error!"),
     };
     let x: &[_] = &[' ', '\t', '\n', '\r'];
-    let result = from_utf8(&out.stdout)
-        .expect("msg")
-        .trim_end_matches(x);
+    let result = from_utf8(&out.stdout).expect("msg").trim_end_matches(x);
     result.to_string()
 }
 
@@ -124,7 +123,12 @@ pub fn update_prefix_and_split(prefix: &str, split: &str) {
 
 fn create_saved_config() -> File {
     // Query for main/master info
-    println!("Config file not found, creating one at {}", get_saved_config_file_path().to_str().expect("should have been str"));
+    println!(
+        "Config file not found, creating one at {}",
+        get_saved_config_file_path()
+            .to_str()
+            .expect("should have been str")
+    );
     println!("Please input what the main branch is [main|master]: ");
 
     let mut line = String::new();
@@ -170,7 +174,8 @@ fn write_saved_config(cfg: SavedConfig) -> File {
 
     let cfg_json = serde_json::to_string(&cfg).expect("error serializing");
 
-    file.write_all(cfg_json.as_bytes()).expect("could not write to cfg file");
+    file.write_all(cfg_json.as_bytes())
+        .expect("could not write to cfg file");
     file.sync_all().expect("should be able to write to disk");
     file.seek(SeekFrom::Start(0)).unwrap();
 
--- src/status/mod.rs
+++ src/status/mod.rs
@@ -1,14 +1,16 @@
-use std::{fs::{self, File, OpenOptions}, io::{Read, Write}};
+use std::{
+    fs::{self, File, OpenOptions},
+    io::{Read, Write},
+    path::PathBuf,
+};
 
 use chrono::prelude::*;
 
 use crate::config;
 
-
-
 #[derive(Debug)]
 struct StatusFile {
-    days: Vec<Day>
+    days: Vec<Day>,
 }
 
 #[derive(Debug, Clone)]
@@ -32,6 +34,7 @@ enum DayEventType {
 
 pub fn list_statuses() {
     let cfg = config::get_full_config();
+    println!("opening status file: {:?}", cfg.status_file);
     let status = parse_status_file(cfg.status_file).unwrap();
     print!("{}", status_to_string(status));
 }
@@ -56,8 +59,13 @@ pub fn write_status(body: String, todo: bool) {
 
     fs::copy(
         cfg.status_file.clone(),
-        format!("{}/{}_{}", cfg.status_file_backup_dir, today.format("%Y-%m-%d"), Utc::now().timestamp()),
-    ).unwrap();
+        cfg.status_file_backup_dir.join(format!(
+            "{}_{}",
+            today.format("%Y-%m-%d"),
+            Utc::now().timestamp()
+        )),
+    )
+    .unwrap();
 
     write_status_file(cfg.status_file, status).unwrap();
 }
@@ -68,7 +76,7 @@ fn create_event(body: String, todo: bool, repo: String) -> DayEvent {
         false => DayEventType::Issue,
     };
 
-    DayEvent{
+    DayEvent {
         typ,
         info: body,
         repo,
@@ -80,7 +88,7 @@ fn today() -> NaiveDate {
     local.naive_local().date()
 }
 
-fn write_status_file(filepath: String, status_file: StatusFile) -> Result<(), anyhow::Error> {
+fn write_status_file(filepath: PathBuf, status_file: StatusFile) -> Result<(), anyhow::Error> {
     let status_str = status_to_string(status_file);
 
     let mut file = OpenOptions::new()
@@ -90,7 +98,8 @@ fn write_status_file(filepath: String, status_file: StatusFile) -> Result<(), an
         .open(filepath)
         .map_err(anyhow::Error::msg)?;
 
-    file.write_all(status_str.as_bytes()).map_err(anyhow::Error::msg)?;
+    file.write_all(status_str.as_bytes())
+        .map_err(anyhow::Error::msg)?;
     file.sync_all().map_err(anyhow::Error::msg)
 }
 
@@ -101,16 +110,15 @@ fn status_to_string(status_file: StatusFile) -> String {
         for event in day.events {
             let repo_suffix = match event.repo.as_str() {
                 "" => "".to_string(),
-                _ => format!(" ({})", event.repo)
+                _ => format!(" ({})", event.repo),
             };
             match event.typ {
                 DayEventType::Issue => {
                     content.push_str(&format!("- {}{}\n", event.info, repo_suffix));
-                },
+                }
                 DayEventType::Todo => {
                     content.push_str(&format!("- TODO: {}{}\n", event.info, repo_suffix));
-                },
-
+                }
             }
         }
         content.push('\n');
@@ -118,18 +126,15 @@ fn status_to_string(status_file: StatusFile) -> String {
     content
 }
 
-
-fn parse_status_file(filename: String) -> Result<StatusFile, anyhow::Error> {
+fn parse_status_file(filename: PathBuf) -> Result<StatusFile, anyhow::Error> {
     let mut file = File::open(filename).map_err(anyhow::Error::msg)?;
 
     let mut buf = String::new();
     file.read_to_string(&mut buf).map_err(anyhow::Error::msg)?;
 
-    let mut status_file = StatusFile{
-        days: vec![],
-    };
+    let mut status_file = StatusFile { days: vec![] };
     let mut initialized = false;
-    let mut current_day = Day{
+    let mut current_day = Day {
         day: NaiveDate::from_ymd(0, 1, 1),
         events: vec![],
     };
@@ -143,7 +148,7 @@ fn parse_status_file(filename: String) -> Result<StatusFile, anyhow::Error> {
             } else {
                 status_file.days.push(current_day.clone());
             }
-            current_day = Day{
+            current_day = Day {
                 day,
                 events: vec![],
             };
@@ -159,7 +164,7 @@ fn parse_status_file(filename: String) -> Result<StatusFile, anyhow::Error> {
 
 fn parse_day_from_line(line: &str) -> Option<NaiveDate> {
     if !line.starts_with('[') {
-        return None
+        return None;
     }
     let t_line = line.trim();
     let date = NaiveDate::parse_from_str(t_line, "[%Y-%m-%d]").unwrap();
@@ -168,18 +173,18 @@ fn parse_day_from_line(line: &str) -> Option<NaiveDate> {
 
 fn parse_event_from_line(line: &str) -> Option<DayEvent> {
     if let Some(stripped) = line.strip_prefix("- TODO: ") {
-        return Some(DayEvent{
+        return Some(DayEvent {
             typ: DayEventType::Todo,
             info: stripped.to_string(),
             repo: "".to_string(),
-        })
+        });
     }
     if let Some(stripped) = line.strip_prefix("- ") {
-        return Some(DayEvent{
+        return Some(DayEvent {
             typ: DayEventType::Issue,
             info: stripped.to_string(),
             repo: "".to_string(),
-        })
+        });
     }
     None
 }
